9e9ca9c2e43423bea7affcad639a581c670e2faf
Marco Vermeulen <marco.vermeulen@and.co.uk>
2011-03-18 14:25:08 +0000

    Moved the Specs to integration folder.

---
+++ test/integration/com/mycodesnippets/UniverseSpec.groovy
@@ -0,0 +1,36 @@
+package com.mycodesnippets
+
+import spock.lang.*
+import grails.plugin.spock.*
+
+class UniverseSpec extends UnitSpec {
+
+    def "in the beginning, there was light"() {
+        given:
+        def moon = Moon.build()
+        def planet = Planet.build()
+        def sun = Sun.build()
+        def galaxy = Galaxy.build()
+        def universe = Universe.build()
+        assert universe.save()
+        
+        and:
+        planet.addToMoons moon
+        sun.addToPlanets planet
+        galaxy.addToSuns sun
+        universe.addToGalaxys galaxy
+        
+        when:
+        def storedUniverse = Universe.get(universe.id)
+        def storedGalaxy = Galaxy.get(galaxy.id)
+        def storedSun = Sun.get(sun.id)
+        def storedPlanet = Planet.get(planet.id)
+        def storedMoon = Moon.get(moon.id)
+        
+        then:
+        storedUniverse.galaxys.contains galaxy
+        storedGalaxy.suns.contains sun
+        storedSun.planets.contains planet
+        storedPlanet.moons.contains moon
+    }
+}
